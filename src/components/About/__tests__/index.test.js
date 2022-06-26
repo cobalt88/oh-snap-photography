3027ee4a0d7f325b2bc12d9ca5d4046ac8cca696
cobalt88 <vincentteune@gmail.com>
2022-06-26 16:54:17 -0400

    6.26.22
    add in second test

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, cleanup } from '@testing-library/react';
-import '@testing-library/jest-dom/extend.expect';
+import '@testing-library/jest-dom/extend-expect';
 import About from '..';
 
 afterEach(cleanup);
@@ -11,5 +11,9 @@ describe('About component', () => {
     render(<About />);
   });
   //Second Test
-  
-})
+  it('matches snapshot DOM node structure', () => {
+    //render about
+    const { asFragment } = render(<About />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
