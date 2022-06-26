b3b03082a08baf190e7a28568632d074eaf5e870
cobalt88 <vincentteune@gmail.com>
2022-06-25 20:43:03 -0400

    6.25.22
    add in nav component
    :

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
-import About from './components/About';
+import './App.css';
+import About from "./components/About/index"
 
 function App() {
   return (
-    <div> 
-      <main>
-        <About></About>
-      </main>
-    </div>
-  );
+    <About />
+  )
 }
 
 export default App;
