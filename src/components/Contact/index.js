ee89c3fed6737325025e430b6c8f7e9cd61181a0
cobalt88 <vincentteune@gmail.com>
2022-07-04 12:31:44 -0400

    7.4.22
    build out form component

---
+++ src/components/Contact/index.js
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+
+function ContactForm() {
+  const [formState, setFormState] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const { name, email, message } = formState;
+
+  function handleChange(e) {
+    setFormState({
+      ...formState,
+      [e.target.name]: e.target.value,
+    });
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(formState);
+  }
+
+  return (
+    <section>
+      <h1>Contact Me</h1>
+      <form id="contact-form" onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name">Name: </label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            defaultValue={name}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email: </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            defaultValue={email}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="message">Message: </label>
+          <textarea
+            id="message"
+            name="message"
+            defaultValue={message}
+            rows="5"
+            onChange={handleChange}
+          ></textarea>
+        </div>
+        <button type="submit">Send</button>
+      </form>
+    </section>
+  );
+}
+
+export default ContactForm;
